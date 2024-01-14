73485fc27628d2e7422af65014531a259e6d53a7
mountaincenter <y.hiroyuki560730@gmail.com>
2024-01-14 09:58:13 +0900

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { CreateWeight } from "@/app/_components/CreateWeight";
 import { type User } from "@/app/types";
 import WeightStats from "@/app/_components/WeightStats";
 import WeightManagement from "@/app/_components/WeightManagement";
-import WebcamComponent from "./_components/WebcamComponent";
 
 export default async function Home() {
   const posts = await api.post.getAllPosts.query();
@@ -41,7 +40,6 @@ export default async function Home() {
           <TodoList posts={posts} />
         </div>
       </div>
-      <WebcamComponent />
     </main>
   );
 }
